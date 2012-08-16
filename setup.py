29ca347dc82177dfacbe9bbb7b6fe5275d6eb399
cage <517020375@qq.com>
2012-08-16 22:29:11 +0800

    bootstrap

--- setup.py
+++ setup.py
@@ -9,3 +9,6 @@ try:
     __import__("django")
 except ImportError:
     os.system("wget http://www.djangoproject.com/m/releases/1.3/Django-1.3.3.tar.gz")
+
+os.system("yum install python-setuptools")
+os.system("sudo apt-get install python-setuptools")
